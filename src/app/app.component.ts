bc31cf1e1b4368a4aa9e871d5b1d86b6fe1a0f1e
daisymae <daisymae@orcadiafarm.com>
2018-04-27 15:24:19 -0500

    Add navigation using events.
    Use knowledge about components and events to manage selection of tabs
    from one component to manage display in another.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedNavigation = 'recipe';
+
+  onNavigate(navigationTab: string) {
+    this.loadedNavigation = navigationTab;
+  }
 }
