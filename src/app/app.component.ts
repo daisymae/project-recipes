89449f2dfd559d7e0016a9b13939db28f40c3829
daisymae <daisymae@orcadiafarm.com>
2018-04-30 00:41:38 -0500

    Clean up
    Additional clean up of event processing no longer needed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedNavigation = 'recipe';
 
-  onNavigate(navigationTab: string) {
-    this.loadedNavigation = navigationTab;
-  }
 }
